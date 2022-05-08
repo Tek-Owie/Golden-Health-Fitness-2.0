4d09846b58899d3f7135253686b3e408bbd2e2ab
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-05-08 14:44:02 +0100

    Added g-tag manager

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
-
 import "../components/all.sass";
 import Layout from "../components/Layout";
 import Features from "../components/Features";
